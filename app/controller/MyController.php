e2fa20b17837724b37cc491308d4c0a84a2d6cf8
tongnian <294932451@qq.com>
2024-09-30 18:29:07 +0800

    修复时差

--- app/controller/MyController.php
+++ app/controller/MyController.php
@@ -4,6 +4,7 @@ namespace app\controller;
 
 use DateTime;
 use app\model\Photo;
+use DateTimeZone;
 use nswl\utils\Redis;
 use support\Cache;
 use support\Request;
@@ -14,28 +15,31 @@ class MyController
     {
         $lin = '0930';
         $tong = '1019';
+
         // 获取当前日期
-        // 获取当前日期
+        $usTimezone = new DateTimeZone('America/New_York');
+        $linNow = new DateTime('now',$usTimezone);
         $now = new DateTime();
 
 // 计算阳历生日
-        $linBirthday = DateTime::createFromFormat('md', $lin);
-        $linBirthday->setDate($now->format('Y'), $linBirthday->format('m'), $linBirthday->format('d'));
+        $linBirthday = DateTime::createFromFormat('md', $lin, $usTimezone);;
+        $linBirthday->setDate($linNow->format('Y'), $linBirthday->format('m'), $linBirthday->format('d'));
         $tongBirthday = DateTime::createFromFormat('md', $tong);
         $tongBirthday->setDate($now->format('Y'), $tongBirthday->format('m'), $tongBirthday->format('d'));
 
 // 如果阳历生日已经过去，计算下一年的日期
-        if ($linBirthday < $now || $tongBirthday < $now) {
+        if ($linBirthday < $linNow || $tongBirthday < $now) {
             $linBirthday->modify('+1 year');
         }
 
 // 计算距离阳历生日还有多少天
         $linDaysRemaining = $now->diff($linBirthday)->days;
+
         $tongDaysRemaining = $now->diff($tongBirthday)->days;
 
         $list = [
-            ['name' => '距离猪猪林的生日还有', 'days' => $linDaysRemaining,'iconClass'=>'http://39.98.115.211:8787/app/admin/upload/img/20240912/66e2b6609949.jpg'],
-            ['name' => '距离猪猪童的生日还有', 'days' => $tongDaysRemaining ,'iconClass'=>'http://39.98.115.211:8787/app/admin/upload/img/20240912/66e2b6577353.jpg'],
+            ['name' => '距离YingLin的生日还有', 'days' => $linDaysRemaining,'iconClass'=>'http://39.98.115.211:8787/app/admin/upload/img/20240912/66e2b6609949.jpg'],
+            ['name' => '距离童超的生日还有', 'days' => $tongDaysRemaining ,'iconClass'=>'http://39.98.115.211:8787/app/admin/upload/img/20240912/66e2b6577353.jpg'],
             ['name' =>  '第一次打电话睡觉','days' => '2024-07-28','iconClass'=>'http://39.98.115.211:8787/app/admin/upload/img/20240925/66f3d36c5605.jpg'],
             ['name' =>  '第一次说我爱你','days' => '2024-08-12','iconClass'=>'http://39.98.115.211:8787/app/admin/upload/img/20240925/66f3d36ccde6.jpg'],
             ['name' =>  '第一次过中秋节','days' => '2024-09-17','iconClass'=>'http://39.98.115.211:8787/app/admin/upload/img/20240918/66ea37559322.jpg'],
